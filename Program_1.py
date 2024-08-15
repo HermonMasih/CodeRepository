2f139f5c7cb2688ce270a12a0c909f16222801f4
Hermon Masih <hermon.masih@gmail.com>
2024-08-15 22:48:47 +0530

    Program_1 Done

---
+++ Program_1.py
@@ -0,0 +1,12 @@
+# Find all occurrences of a substring in a given string by ignoring the case
+# Input: str1 = "Welcome to USA. usa awesome, isn't it?"
+# output: The USA count is: 2
+
+def count_occurence(str1, substr):
+    return str1.count(substr)
+
+str_1=input("Enter the string\n").lower()
+substr=input("Enter the substring\n").lower()
+
+cnt=count_occurence(str_1, substr)
+print(f'The {substr.upper()} count is: {cnt}')
