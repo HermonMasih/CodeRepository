6ae3ca342b9c05614d7ba8e1b3fde6cde31c220b
Hermon Masih <hermon.masih@gmail.com>
2024-08-23 02:21:42 +0530

    Program_2 Done

---
+++ Program_2.py
@@ -0,0 +1,12 @@
+# s1 = "Ynf"
+# s2 = "PYnative"
+
+# print(s1 in s2)
+
+# str1 = "Emma is a data scientist who knows Python. Emma works at google."
+#  Find the last position of a given substring
+
+str1 = "Emma is a data scientist who knows Python. Emma works at google."
+
+idx=str1.rindex("Emma")
+print(f'Last occurrence of Emma starts at index {idx}')
